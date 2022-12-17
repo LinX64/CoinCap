d969c2f31b36e578635b14c68f20bd384a7bee5b
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-12-17 13:26:24 +0100

    Added temporary test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,18 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.window.manager)
     implementation(libs.androidx.profileinstaller)
+
+    // TODO: maybe adding this as a separate module?
+    api(libs.junit4)
+    api(libs.androidx.test.core)
+    api(libs.kotlinx.coroutines.test)
+    api(libs.turbine)
+
+    api(libs.androidx.test.espresso.core)
+    api(libs.androidx.test.runner)
+    api(libs.androidx.test.rules)
+    api(libs.androidx.compose.ui.test)
+    api(libs.hilt.android.testing)
+
+    debugApi(libs.androidx.compose.ui.testManifest)
 }
