94a2ee92e703c793452a36db4febb0c2d3b61902
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-12-17 15:40:21 +0100

    Fixing the test

--- app/src/androidTest/java/com/client/coincap/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/client/coincap/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.client.coincap", appContext.packageName)
+        assertEquals("com.client.coincap.debug", appContext.packageName)
     }
 }
