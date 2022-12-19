15bf4bd8ba042464c60f3c9d50bf7ac89b70b045
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-12-19 14:27:32 +0100

    Fixed the sample test

--- ui/search/src/androidTest/java/com/client/search/ExampleInstrumentedTest.kt
+++ ui/search/src/androidTest/java/com/client/search/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.client.search.test", appContext.packageName)
+        assertEquals("com.client.coincap.core.search.test", appContext.packageName)
     }
 }
