602f761b167cac0c65b80a1b85fc1d3308b1103c
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-12-25 22:34:35 +0100

    Refactor + removing extra files

--- core/data/src/main/java/com/client/data/util/Const.kt
+++
@@ -1,6 +0,0 @@
-package com.client.data.util
-
-object Const {
-    const val BASE_URL = "https://api.coincap.io/v2/"
-    const val DELAY = 5000L
-}
---
+++ core/data/src/main/java/com/client/data/util/Consts.kt
@@ -0,0 +1,11 @@
+package com.client.data.util
+
+object Consts {
+    const val RATES_API = "https://api.coincap.io/v2/"
+    const val LOCAL_RATES_API = "https://bonbast-api.deta.dev/"
+    const val CryptoIconUrl = "https://coinicons-api.vercel.app/api/icon/"
+
+    const val NORMAL = "Normal"
+    const val LOCAL_CURRENCY = "Local"
+    const val DELAY = 5000L
+}
