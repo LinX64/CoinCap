ed3f0920b23389172c63d058aee0d6a947951f2a
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-12-28 00:56:02 +0100

    Added detektsPlugin from version catalog

--- build-logic/convention/src/main/kotlin/KotlinDetektConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinDetektConventionPlugin.kt
@@ -1,14 +1,17 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class KotlinDetektConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("io.gitlab.arturbosch.detekt")
 
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
-                add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
+                "detektPlugins"(libs.findLibrary("detekt-formatting").get())
             }
         }
     }
