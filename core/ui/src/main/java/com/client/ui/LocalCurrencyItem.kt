c37e2e8a6355f998c5228f9831e41f83ff940164
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-12-29 13:06:58 +0100

    Detekt apply

--- core/ui/src/main/java/com/client/ui/LocalCurrencyItem.kt
+++ core/ui/src/main/java/com/client/ui/LocalCurrencyItem.kt
@@ -9,14 +9,12 @@ import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.toColorInt
 import com.client.coincap.core.ui.R
 import com.client.common.util.CountryFlags
 import com.client.common.util.formatToPrice
