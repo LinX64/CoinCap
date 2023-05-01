622e699fe6b39a321a07d2742523dcbb551fac28
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-05-01 19:52:35 +0200

    Apply detekt

--- app/src/main/java/com/client/coincap/ui/CoinCapApp.kt
+++ app/src/main/java/com/client/coincap/ui/CoinCapApp.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.consumedWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
