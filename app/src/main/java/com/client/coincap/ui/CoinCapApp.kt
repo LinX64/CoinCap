b792d402da906aafbcd210ff712c657980824c0b
Mohsen Rzna <ash.wxrz@hotmail.com>
2023-12-10 16:03:49 +0100

    Apply detekt

--- app/src/main/java/com/client/coincap/ui/CoinCapApp.kt
+++ app/src/main/java/com/client/coincap/ui/CoinCapApp.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
--- app/src/main/java/com/client/coincap/ui/TopAppBar.kt
+++ app/src/main/java/com/client/coincap/ui/TopAppBar.kt
@@ -3,8 +3,13 @@ package com.client.coincap.ui
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
